12678d8a648b72d6bfdc939b22f5f41393564a25
Kamily <kamily7803@gmail.com>
2025-03-08 19:19:19 -0300

    build: add Vue.js to Laravel project with Vite

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-5">
+        <h1 class="text-2xl font-bold">Hello, Vue com Laravel!</h1>
+        <p>Essa é uma aplicação Laravel com Vue.js 🎯</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
