58bd230236c0dfc7b1e0e5ec39da6d07d64535cb
Kamily <kamily7803@gmail.com>
2025-03-08 16:03:15 -0300

    feat: add base repository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+class BaseRepository
+{
+  public function getAll()
+  {
+    return $this->model->get();
+  }
+
+  public function getAttributesById(array $attributes, int $id)
+  {
+    return $this->model->select($attributes)
+      ->where('id', $id)
+      ->first();
+  }
+
+  public function getById(int $id)
+  {
+    return $this->model->find($id);
+  }
+
+  public function store(array $data)
+  {
+    return $this->model->create($data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->model->where('id', $id)->delete();
+  }
+}
